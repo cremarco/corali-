70070ed22b3351d23fd5442f080a6f1f4cf751c9
Marco <marco.cremaschi@unimib.it>
2026-06-01 18:25:20 +0200

    Add new HTML page and JavaScript functionality for mobile menu and reveal animations
    - Created `inspiration.html` with a complete layout for Ristorante Coralì, including sections for the hero, philosophy, menu highlights, and details.
    - Added new images for the hero and locale sections.
    - Implemented `site.js` to handle mobile menu toggle and reveal animations using Intersection Observer.

--- Coralì/immagini/footer/js/src/site.js
+++
@@ -1,7 +0,0 @@
-
-jQuery(document).ready(function() {
-    var url      = window.location.href;
-    console.log(url);
-    jQuery(".nav-link[href='"+url+"']").addClass("nav-link-active");
-    
-})
